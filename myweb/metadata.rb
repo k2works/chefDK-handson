cb87ea11a2f8ae38acf28c850df02076b854b93e
Your Name <you@example.com>
2015-03-06 20:27:27 +0900

    setu .kitchen.yml

---
+++ myweb/metadata.rb
@@ -0,0 +1,8 @@
+name             'myweb'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures myweb'
+long_description 'Installs/Configures myweb'
+version          '0.1.0'
+
