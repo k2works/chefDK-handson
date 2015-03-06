c62939a693ae3e65fa97eaad3ef63d6312c526cd
Your Name <you@example.com>
2015-03-06 21:09:34 +0900

    install nginx

--- myweb/recipes/default.rb
+++ myweb/recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+package 'nginx'
+
+service 'nginx' do
+  action [:start]
+end
--- myweb/spec/unit/recipes/default_spec.rb
+++ myweb/spec/unit/recipes/default_spec.rb
@@ -4,20 +4,12 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+
 require 'spec_helper'
 
 describe 'myweb::default' do
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-  context 'When all attributes are default, on an unspecified platform' do
-
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      chef_run # This should not raise an error
-    end
-
-  end
+  it { expect(chef_run).to install_package('nginx') }
+  it { expect(chef_run).to start_service('nginx') }
 end
